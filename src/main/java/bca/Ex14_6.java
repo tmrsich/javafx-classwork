c57743b4367a621062ced32d8e53c49a74600b00
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2023-09-18 11:24:48 -0400

    Exercise 14.6

---
+++ src/main/java/bca/Ex14_6.java
@@ -0,0 +1,20 @@
+package bca;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Ex14_6 extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    
+    
+
+
+
+    
+}
+
+    public static void main(String[] args) {
+        Application.launch(args);
+        }
+}
